847ca57efa3b65874b117143964d4bb551b2e080
vasanth2030 <vasanths2030@gmail.com>
2022-04-21 21:23:24 +0530

    java ppt and code added

---
+++ Java code/basicjava/BasicJava.java
@@ -0,0 +1,53 @@
+package com.vasanth.basicjava;
+
+import java.util.Scanner;
+
+public class BasicJava {
+	public static void main(String[] args) {
+		
+//		String name;
+//		int age;
+//		float height;   //double
+//		
+//		System.out.println("Enter your name: ");
+//		name= sc.nextLine();
+//		
+//		System.out.println("Enter your age: ");
+//		age= sc.nextInt();
+//		
+//		System.out.println("Enter your height: ");
+//		height= sc.nextFloat();
+//		
+//		System.out.println();
+//		System.out.println();
+//		System.out.println();
+//		
+//		System.out.println("Hello World!");
+//		System.out.println("Name: "+name);
+//		System.out.println("Age: "+age+" years old.");
+//		System.out.println("Height: "+height+" cm");
+		
+		Scanner sc=new Scanner(System.in); 
+		int a=10;
+		int b=2;
+
+//		System.out.println(a+b);
+//		System.out.println(a-b);
+//		System.out.println(a*b);
+//		System.out.println(a/b);
+//		System.out.println(a%b);
+		
+
+//		System.out.println(--a); 
+//		System.out.println(a);
+		
+		a+=b;    // a=a+b
+		a-=b;    // a=a-b
+		a*=b;    // a=a*b
+		a/=b;    // a=a/b
+		a%=b;    // a=a%b
+		System.out.println(a);
+		
+	}
+	
+}
